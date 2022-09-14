168331a3ec393fb47711951e1c8e6e38f51fbd25
NIKHILKEDAR8805 <112891692+NIKHILKEDAR8805@users.noreply.github.com>
2022-09-14 23:21:42 +0530

    assignment01
    question03

---
+++ assignment1_ques3.py
@@ -0,0 +1,17 @@
+numbers = (1,2,3,4,5,6,7,8,9)
+print("even and odd tup item =",numbers)
+
+even_count = odd_count = 0
+
+for i in range(len(numbers)):
+    if (numbers[i] % 2 == 0):
+        even_count = even_count+ 1
+    else:
+        odd_count =odd_count+ 1
+
+print("even count:",even_count)
+print("odd count:",odd_count)
+
+    
+    
+     
