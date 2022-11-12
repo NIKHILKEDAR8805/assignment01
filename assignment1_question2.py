480463a1cd96187a9c6033f8b464818d99687801
NIKHILKEDAR8805 <112891692+NIKHILKEDAR8805@users.noreply.github.com>
2022-11-12 12:19:52 +0530

    Update assignment1_question2.py

--- assignment1_question2.py
+++ assignment1_question2.py
@@ -1,3 +1,8 @@
 s = "edyoda"
 for i in range((len(s)-1),-1,-1):
     print(s[i],end="")
+    
+
+# given_string=input('enter word:')
+# mirrored_string=given_string[::-1]
+# print("mirrored_string:",mirrored_string)
