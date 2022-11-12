bbb82fd4edf6fc76078c22033d07be20523f982c
NIKHILKEDAR8805 <112891692+NIKHILKEDAR8805@users.noreply.github.com>
2022-11-12 15:20:14 +0530

    Update assignment1__que1.py

--- assignment1__que1.py
+++ assignment1__que1.py
@@ -1,15 +1,12 @@
-num=int(input("enter num : "))
-n1 ,n2 = 0,1
-sum= 0
+num = int(input("enter number :"))
+n1,n2=0,1
+sum=0
 if num<=0:
-    print('enter num greater than 0')
+    print("enter num greater than zero")
 else:
-    print("fibonacci:",end="")
-    for i in range (0,num):
-        print(sum,end="")
+    print("fibonacci series :",end="")
+    for i in range(0,num):
         n1=n2
         n2=sum
         sum=n1+n2
-
-
-        
+        print(sum,end="")
